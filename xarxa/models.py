01bbb659b3a791a911734ed1d3cce74fe4d9a063
avaquero <avaquero1469@gmail.com>
2014-05-13 18:41:15 +0200

    models creats

---
+++ xarxa/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Perfil(models.Model):
+    usuari = models.OneToOneField(User)
+    nom = models.CharField(max_length=30)
+    cognoms = models.CharField(max_length=30)
+    dataNaix = models.DateField()
+    equip = models.CharField(max_length=30)
+    
+class Amic(models.Model):
+    usu1 = models.ForeignKey(Perfil)
+    usu2 = models.ForeignKey(Perfil)
+    
+class Publicacio(models.Model):
+    usuari = models.ForeignKey(Perfil)
+    privat = models.BooleanField()
+    dataHora = models.DateTimeField()
+    urlImatge = models.TextField()
+    text = models.TextField()
+    
+class Comentari(models.Model):
+    publicacio = models.ForeignKey(Publicacio)
+    usuari = models.ForeignKey(Perfil)
+    comentari = models.TextField()
+    dataHora = models.DateTimeField()
