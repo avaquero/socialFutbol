6a05ea12647814d13ae90b71d1057be4e672f741
avaquero <avaquero1469@gmail.com>
2014-05-13 18:53:41 +0200

    setings canviat taula amic no funciona

--- socialFutbol/settings.py
+++ socialFutbol/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'xarxa',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +58,12 @@ WSGI_APPLICATION = 'socialFutbol.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'xarxaSocial',
+        'USER': 'root',
+        'PASSWORD': 'vg5218',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
